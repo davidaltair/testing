863d5d11a7ac56d55ddee87e6acc2ce9953bf21f
davidaltair <davidlucenabasketlife@gmail.com>
2022-12-29 17:19:07 +0100

    Subida de dependencias de groovy y nuevo archivo v2

---
+++ jenkinsfile - V2.groovy
@@ -0,0 +1,31 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Hello') {
+            steps {
+                echo 'Archivo de jenkinsfile de GitHub'
+            }
+        }
+        stage('Clonado del proyecto') {
+            steps {
+                echo 'Se clona el proyecto'
+                git branch: 'main', credentialsId: '663f2944-7e26-4e0c-8349-07e2b8338417', url: 'https://github.com/davidaltair/testing.git'
+            }
+        }
+        stage('Informacion del sistema') {
+            steps {
+                echo 'version de java'
+                sh "java --version"
+                echo 'version de maven'
+                sh "mvn --version"
+            }
+        }
+        stage('Clonado del proyecto de ejemplo') {
+            steps {
+                echo 'Clonado del proyecto de ejemplo'
+                git 'https://github.com/jenkins-docs/simple-java-maven-app.git'
+            }
+        }
+    }
+}
