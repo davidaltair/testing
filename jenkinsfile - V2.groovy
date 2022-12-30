701b076457b57cf2cd9584f3942e71b38a8d828a
davidaltair <davidlucenabasketlife@gmail.com>
2022-12-30 12:21:54 +0100

    Compilado del proyecto

--- jenkinsfile - V2.groovy
+++ jenkinsfile - V2.groovy
@@ -9,6 +9,7 @@ pipeline {
         }
         stage('Clonado del proyecto') {
             steps {
+                sh 'rm -r /var/lib/jenkins/workspace/Proyecto_Jenkinsfile'
                 echo 'Se clona el proyecto'
                 //git branch: 'main', credentialsId: '663f2944-7e26-4e0c-8349-07e2b8338417', url: 'https://github.com/davidaltair/testing.git'
                 sh ' git clone https://github.com/davidaltair/testing.git /var/lib/jenkins/workspace/Proyecto_Jenkinsfile'
@@ -24,6 +25,7 @@ pipeline {
         }
         stage('Clonado del proyecto de ejemplo') {
             steps {
+                sh 'rm -r /var/lib/jenkins/workspace/Proyecto_Ejemplo'
                 echo 'Clonado del proyecto de ejemplo, archivo groovy'
                 //git 'https://github.com/jenkins-docs/simple-java-maven-app.git'
                 sh ' git clone https://github.com/jenkins-docs/simple-java-maven-app.git /var/lib/jenkins/workspace/Proyecto_Ejemplo'
